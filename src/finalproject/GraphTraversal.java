ea401e05421eb1d42545f2d1fd2609ea0f2c605b
joukakelian <117304979+joukakelian@users.noreply.github.com>
2022-12-11 18:40:51 -0500

    lvl 1 complete

--- src/finalproject/GraphTraversal.java
+++ src/finalproject/GraphTraversal.java
@@ -11,15 +11,37 @@ public class GraphTraversal {
 	// input Tile start, traverse map find all reachable tiles, return ArrayList w Tiles in order of visitation
 	//TODO level 1: implement BFS traversal starting from s
 	public static ArrayList<Tile> BFS(Tile s) {
-		// use queue version of dfs to traverse
-		// whenever visit, add tile to an arraylist
+		HashSet<Tile> visitedMap = new HashSet<>();
+		ArrayList<Tile> toVisit = new ArrayList<>();
+		ArrayList<Tile> visited = new ArrayList<>();
 		
+		toVisit.add(s);
+		visited.add(s);
 		
-		return null;
+		while (!toVisit.isEmpty()) {
+			Tile cur;
+			
+			if (toVisit.size() != 1)
+				cur = toVisit.remove(toVisit.size()-2); // cur is previous tile
+			else cur = toVisit.remove(0);
+			
+			if (!visitedMap.contains(cur) && !toVisit.contains(cur)) {
+				visitedMap.add(cur);
+				
+				for (var tile : cur.neighbors) {
+					if (!visited.contains(tile) && tile.isWalkable()) {
+						visited.add(tile);
+						toVisit.add(tile); // push
+					}
+					
+				}
+			}
+		}
+		return visited;
 	}
 
+	// ############################################ DFS ################################################################
 
-	//TODO level 1: implement DFS traversal starting from s
 	public static ArrayList<Tile> DFS(Tile s) {
 		HashSet<Tile> visitedMap = new HashSet<>(); // if no entry for a vertex, not visited
 		ArrayList<Tile> toVisit = new ArrayList<>(); // like stack but need to manually add and remove
@@ -28,25 +50,19 @@ public class GraphTraversal {
 		toVisit.add(s);
 		
 		while (!toVisit.isEmpty()) {
-			System.out.println("toVisit: " + toVisit);
 			Tile cur = toVisit.remove(toVisit.size()-1); // pop
-			System.out.println("cur is " + cur);
+			
 			if (!visited.contains(cur)) visited.add(cur);
 			
 			if (!visitedMap.contains(cur) && !toVisit.contains(cur)) {
 				visitedMap.add(cur);
-				System.out.println("before for: " + cur);
 				for (var tile : cur.neighbors) {
 					if (!toVisit.contains(tile) && tile.isWalkable()) {
 						toVisit.add(tile); // push
 					}
-					System.out.println("adding tile: " + tile);
 				}
-				
 			}
 		}
-		System.out.println(visited);
-		
 		return visited;
 	}
 
