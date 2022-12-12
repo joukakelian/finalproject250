ceae2cdfd65452e3a6a12d58e994ece9fa1ad61b
joukakelian <117304979+joukakelian@users.noreply.github.com>
2022-12-12 12:38:08 -0500

    lvl 2 complete (fixed bug)

--- src/finalproject/Graph.java
+++ src/finalproject/Graph.java
@@ -12,21 +12,22 @@ public class Graph {
     private ArrayList<Edge> edgeList = new ArrayList<>();
     private ArrayList<Tile> verticesList = new ArrayList<>();
     
-	public Graph(ArrayList<Tile> vertices) {
-	    this.verticesList = vertices;
-	}
-	
-    public void addEdge(Tile origin, Tile destination, double weight){
+    public Graph(ArrayList<Tile> vertices) {
+        this.verticesList = vertices;
+    }
+    
+    public void addEdge(Tile origin, Tile destination, double weight) {
         Edge e = new Edge(origin, destination, weight);
         edgeList.add(e);
         origin.addNeighbor(destination);
+        destination.addNeighbor(origin);
     }
     
-	public ArrayList<Edge> getAllEdges() {
-		return edgeList;
-	}
-  
-	public ArrayList<Tile> getNeighbors(Tile t) {
+    public ArrayList<Edge> getAllEdges() {
+        return edgeList;
+    }
+    
+    public ArrayList<Tile> getNeighbors(Tile t) {
         ArrayList<Tile> tNeighbors = new ArrayList<>();
         for (var vertex : verticesList) {
             if (vertex == t) {
@@ -37,15 +38,15 @@ public class Graph {
                 }
             }
         }
-    	return tNeighbors;
+        return tNeighbors;
     }
-	
-	// TODO level 2: return total cost for the input path
-	public double computePathCost(ArrayList<Tile> path) {
+    
+    // TODO level 2: return total cost for the input path
+    public double computePathCost(ArrayList<Tile> path) {
         double pathCost = 0.0;
-		for (int i = 1; i < path.size()-1; i++) {
-            for (int j = 0; j < edgeList.size()-1; j++) {
-                Tile src = path.get(i-1);
+        for (int i = 1; i < path.size(); i++) {
+            for (int j = 0; j < edgeList.size(); j++) {
+                Tile src = path.get(i - 1);
                 Tile dest = path.get(i);
                 if (edgeList.get(j).getStart() == src && edgeList.get(j).getEnd() == dest) {
                     pathCost += edgeList.get(j).weight;
@@ -53,21 +54,21 @@ public class Graph {
             }
         }
         return pathCost;
-	}
-	
-   
-    public static class Edge{
-    	Tile origin;
-    	Tile destination;
-    	double weight;
-
-        public Edge(Tile s, Tile d, double cost){
-        	this.origin = s;
+    }
+    
+    
+    public static class Edge {
+        Tile origin;
+        Tile destination;
+        double weight;
+        
+        public Edge(Tile s, Tile d, double cost) {
+            this.origin = s;
             this.destination = d;
             this.weight = cost;
         }
         
-        public Tile getStart(){
+        public Tile getStart() {
             return this.origin;
         }
         
@@ -90,21 +91,20 @@ public class Graph {
         vertices.add(vertex4);
     
     
-    
         Graph weightedGraph = new Graph(vertices);
     
-        weightedGraph.addEdge(vertex1,vertex2,5);
-        weightedGraph.addEdge(vertex2,vertex3,5);
-        weightedGraph.addEdge(vertex3,vertex4,5);
-        weightedGraph.addEdge(vertex4,vertex1,5);
+        weightedGraph.addEdge(vertex1, vertex2, 5);
+        weightedGraph.addEdge(vertex2, vertex3, 5);
+        weightedGraph.addEdge(vertex3, vertex4, 5);
+        weightedGraph.addEdge(vertex4, vertex1, 5);
     
     
-        System.out.print("Path length from tile 1 to tile 4:");
+        System.out.print("Path length from tile 1 to tile 4: ");
         System.out.println(weightedGraph.computePathCost(vertices));
     
-        for(Edge edge: weightedGraph.getAllEdges())
-            System.out.println("Edge linking: " + edge.origin +" and " + edge.destination
-                    + " with weight "+ edge.weight);
+        for (Edge edge : weightedGraph.getAllEdges())
+            System.out.println("Edge linking: " + edge.origin + " and " + edge.destination
+                    + " with weight " + edge.weight);
     }
     
 }
